3491933000cea53a0e869843ad6dbab48399ace2
Vaibhav yadav <Vaibhav160404@gmail.com>
2022-11-10 02:53:19 +0530

    Added Logo and Colors

--- app/src/main/java/com/vaibhav/konnection/MainActivity.kt
+++ app/src/main/java/com/vaibhav/konnection/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.vaibhav.konnection.ui.theme.KonnectionTheme
@@ -28,16 +36,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             KonnectionTheme {
 // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+
+
                     navController = rememberNavController()
                     NavGraph(
                         navController = navController,
                         sharedViewModel = sharedViewModel
                     )
                 }
+
+
             }
         }
     }
--- app/src/main/java/com/vaibhav/konnection/screen/MainScreen.kt
+++ app/src/main/java/com/vaibhav/konnection/screen/MainScreen.kt
@@ -1,20 +1,31 @@
 package com.vaibhav.konnection.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.vaibhav.konnection.nav.Screens
-
+import com.vaibhav.konnection.ui.theme.Teal200
+import com.vaibhav.konnection.R.drawable.kiit
 @Composable
 fun MainScreen(
     navController: NavController,
 ) {
+
+
+
+
     Column(
         modifier = Modifier
             .padding(start = 50.dp, end = 50.dp)
@@ -23,25 +34,49 @@ fun MainScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // get user data Button
+        Text(
+            text = "KIIT Talent Konnections",
+            style = MaterialTheme.typography.h3,
+            color = Color.Green,
+            fontSize = 35.sp
+        )
+
+
+
+        Divider(thickness = 3.dp)
+        Spacer(modifier = Modifier.height(20.dp))
+
         Button(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .height(70.dp)
+                .width(150.dp),
+            shape= RectangleShape,
             onClick = {
                 navController.navigate(route = Screens.GetDataScreen.route)
-            }
+            }, colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.LightGray,
+                contentColor = Color.Magenta)
         ) {
-            Text(text = "Get User Data")
+            Text(text = "Get Student Data")
         }
 
         // add user data Button
         OutlinedButton(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .height(70.dp)
+                .width(150.dp),
             onClick = {
                 navController.navigate(route = Screens.AddDataScreen.route)
-            }
+            }, colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.LightGray,
+                contentColor = Color.Magenta)
         ) {
-            Text(text = "Add User Data")
+            Text(text = "Add Student Data")
         }
+
     }
 }
+
+
 
 
